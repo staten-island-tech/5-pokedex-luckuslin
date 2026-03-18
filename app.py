5a9debb27a71a2c2ad99a5ad3c2b5485e120818d
luckuslin <lukasl18@nycstudents.net>
2026-03-18 09:24:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,11 +27,154 @@ data = json.load(pokedex)
 everypokemon("English") """
 
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-def pkmontype(type):
-    for index,item in enumerate(data):
-            if (item["type"]) == ["Fire"]:
-                  print(item["Fire"])
-pkmontype("f")
+def pkmontype(type,secondary):
+    if type == "Fire" or "fire":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Fire",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Fire"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Grass" or "grass":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Grass",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Grass"]:
+                 print(item["name"]["english"])
+                 print(item["type"]) 
+    elif type == "Normal" or "normal":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Normal",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Normal"]:
+                 print(item["name"]["english"])
+                 print(item["type"])    
+    elif type == "Fighting" or "fighting":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Fighting",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["fighting"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Flying" or "flying":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Flying",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Flying"]:
+                 print(item["name"]["english"])
+                 print(item["type"])   
+    elif type == "Poison" or "poison":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Poison",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Poison"]:
+                 print(item["name"]["english"])
+                 print(item["type"])  
+    elif type == "Ground" or "ground":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Ground",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Ground"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Rock" or "rock":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Rock",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Rock"]:
+                 print(item["name"]["english"])
+                 print(item["type"])  
+    elif type == "Bug" or "bug":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Bug",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Bug"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Ghost" or "ghost":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Ghost",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Ghost"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Steel" or "steel":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Steel",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Steel"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Water" or "water":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Water",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Water"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Electric" or "electric":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Electric",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Electric"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Pyschic" or "Pyschic":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Pyschic",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Pyschic"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Ice" or "Ice":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Ice",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Ice"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Dragon" or "dragon":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Dragon",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Dragon"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Dark" or "dark":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Dark",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Dark"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type == "Fairy" or "fairy":
+        for index,item in enumerate(data):
+            if (item["type"]) == ["Fairy",secondary]:
+                  print(item["name"]["english"])
+                  print(item["type"])
+            elif secondary == "0" and item["type"]== ["Fairy"]:
+                 print(item["name"]["english"])
+                 print(item["type"])
+    elif type and secondary != pokedex:
+         print("Nothing")
+pkmontype("Fire", "0")
 
 
 
